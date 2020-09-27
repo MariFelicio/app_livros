7592ddcef7aa10b26f373641c79b1e220653db6a
MariFelicio <marifelicio_mf@hotmail.com>
2020-09-27 10:38:26 -0300

    Trabalho de Tópicos

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from './src/pages/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ title: "Livros",
+        headerTintColor: 'white',
+        headerStyle: {
+            backgroundColor: '#6ca2f7',
+            borderBottomWidth: 1,
+            borderBottomColor: '#C5C5C5',
+        },
+        headerTitleStyle: {
+            color: 'white',
+            fontSize: 30,
+        } }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ); 
+  }
---
+++ src/pages/LoginScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {View, TextInput, Button, ActivityIndicator } from 'react-native';
+import FormRow from '../components/FormRow';
+import firebase from '@firebase/app';
+import '@firebase/auth';
+
+
+export default class LoginPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mail: '',
+      password: '',
+      isLoading: false,
+    }
+  }
+  componentDidMount(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyAOTGbQ6IpdYjOg95zg39uf3A3SaDoocqo",
+      authDomain: "livros-b80c0.firebaseapp.com",
+      databaseURL: "https://livros-b80c0.firebaseio.com",
+      projectId: "livros-b80c0",
+      storageBucket: "livros-b80c0.appspot.com",
+      messagingSenderId: "370817555715",
+      appId: "1:370817555715:web:18b4cc016e069d28019d36",
+      measurementId: "G-WGRD555CMR"
+    };
+    // Initialize Firebase
+    //firebase.initializeApp(firebaseConfig);   
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp({ /* suas configurações aqui */ });
+    }
+  }
+  onChangeHandler(field, value){
+    this.setState({
+      [field]: value
+    });
+  }
+  tryLogin(){
+    this.setState({isLoading: true});
+    const {mail, password} = this.state
+    firebase.auth().signInWithEmailAndPassword(mail, password).catch((error) => {
+        console.log('Usuário não encontrado', error);
+      })
+      .then(() => this.setState({isLoading: false}))
+  }
+  renderButton(){
+    if(this.state.isLoading)
+      return <ActivityIndicator/>;
+    return( 
+      <Button 
+        title="Entrar"
+        onPress={() => this.tryLogin()}/>
+      );
+  }
+  render() {
+    return(
+      <View>
+        <FormRow>
+           <TextInput 
+              placeholder="email"
+              value={this.state.mail}
+              onChangeText={value => this.onChangeHandler('mail', value)}
+           />
+        </FormRow>
+        <FormRow>
+           <TextInput 
+              placeholder="senha"
+              secureTextEntry
+              value={this.state.password}
+              onChangeText={value => this.onChangeHandler('password', value)}
+          />
+        </FormRow>
+        {this.renderButton() }          
+      </View>
+    )
+  }
+}
