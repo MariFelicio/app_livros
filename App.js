5ab8d03bf3854df1724e9292a60c3d0fa4daf6cc
MariFelicio <marifelicio_mf@hotmail.com>
2020-10-14 14:08:39 -0300

    corrigindo app.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings([
   'Setting a timer for a long period of time',
   'YellowBox has been replaced with LogBox',
-  'source.uri should not be an empty string'
+  'source.uri should not be an empty string',
 ]);
 
-export default from './src/LivrosApp';
+export {default} from './src/LivrosApp';
